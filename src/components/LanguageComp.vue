584e3a4743f9a5ec0d60668b3032f019e2cb8ab0
siddaws <380637541873.a.9@gmail.com>
2024-07-26 18:17:17 +0300

    Your commit message

--- src/components/LanguageComp.vue
+++ src/components/LanguageComp.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="bg-white p-4 rounded-lg shadow-md mt-2">
       <h3 class="text-white bg-black text-2xl p-2 text-lg font-bold">Мови</h3>
-      <ul class="mt-2">
-        <li v-for="language in languages" :key="language.name" class="flex justify-between text-black text-lg mx-80  mt-5">
+      <ul class="mt-2 w-80 m-auto">
+        <li v-for="language in languages" :key="language.name" class="flex justify-between text-black text-lg mx-60  mt-5">
           <span>{{ language.name }}</span>
           <span>{{ language.level }}</span>
         </li>
